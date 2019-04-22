c38d07338c4e5dcc589e6db5df25ad99564bfe80
Gettz <49850415+Gettz@users.noreply.github.com>
2019-04-22 08:09:16 -0500

    Update ESATracker.py

--- ESATracker.py
+++ ESATracker.py
@@ -5,13 +5,13 @@ from decimal import Decimal
 version = '2.5'
 
 
-def main():
+def main():  # On run function, header and opening query
     print("ESA Donation Tracker Version {0}".format(version) + "\n=================================")
     event = input("What is your Event ID (2 digit number): ")
     validity(event)
 
         
-def validity(event)
+def validity(event)  # Checks to see if input was in a valid format
     try:
         if int(event) and len(event) == 2:
             agg(event)
@@ -26,9 +26,9 @@ def validity(event)
 
         
 def agg(eventid):  # Main function, holds all logic for the tracker
-    total = Decimal(0.00)  # aggregate donation total pulled from the ESA site.
-    currency = Decimal(0.00)  # the donation total but correctly formatted to 2 decimal places.
-    output = Decimal(0.00)  # the amount currently saved in the output.txt file, useful for update checks.
+    total = Decimal(0.00)  # Aggregate donation total pulled from the ESA site.
+    currency = Decimal(0.00)  # The donation total but correctly formatted to 2 decimal places.
+    output = Decimal(0.00)  # The amount currently saved in the output.txt file, useful for update checks.
     first = True  # Bool to see if first run of script
     h = {"User-Agent": "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US;"
          "rv:1.9.2.8) Gecko/20100722 Firefox/3.6.8 GTB7.1 (.NET CLR 3.5.30729)",
